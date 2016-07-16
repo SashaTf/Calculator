86b261989b857cff7070db2003206c933deefb0c
SashaTf <sashulya.tarantsova@gmail.com>
2016-07-16 23:32:22 +0600

    изменения после субботы

--- app/src/main/java/ru/appcampsiberia/calculator/MainActivity.java
+++ app/src/main/java/ru/appcampsiberia/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.appcampsiberia.calculator;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +27,26 @@ public class MainActivity extends AppCompatActivity {
     private Button button1;
     private Button button2;
     private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button0;
+    private Button buttonC;
+    private Button buttonMin;
+    private Button buttonDiv;
+    private Button buttonPow;
     private Button buttonAdd;
     private Button buttonMul;
     private Button buttonDone;
     private TextView resultView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("button 1");
                 a = a * 10 + 1;
                 render();
             }
@@ -55,6 +77,77 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 a = a * 10 + 3;
+                render();
+            }
+        });
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 4;
+                render();
+            }
+        });
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 5;
+                render();
+            }
+        });
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 6;
+                render();
+            }
+        });
+
+        button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 7;
+                render();
+            }
+        });
+
+        button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 8;
+                render();
+            }
+        });
+
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 9;
+                render();
+            }
+        });
+
+        button0 = (Button) findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 10 + 0;
+                render();
+            }
+        });
+
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a * 0;
                 render();
             }
         });
@@ -81,6 +174,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonPow = (Button) findViewById(R.id.buttonPow);
+        buttonPow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b = a;
+                a = 0;
+                a = b*b;
+
+            }
+        });
         buttonDone = (Button) findViewById(R.id.buttonDone);
         buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,10 +201,52 @@ public class MainActivity extends AppCompatActivity {
         });
 
         resultView = (TextView) findViewById(R.id.resultView);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void render() {
         resultView.setText(Long.toString(a));
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://ru.appcampsiberia.calculator/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://ru.appcampsiberia.calculator/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
